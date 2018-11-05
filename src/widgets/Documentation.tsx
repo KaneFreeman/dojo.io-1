0361bfebc427e2c9cd6e51c92cfd19916c6e6fad
Matt Gadd <mgadd@sitepen.com>
2018-11-05 14:24:04 +0000

    get tutorial list

--- src/widgets/Documentation.tsx
+++ src/widgets/Documentation.tsx
@@ -4,15 +4,18 @@ import Outlet from '@dojo/framework/routing/Outlet';
 import Link from '@dojo/framework/routing/ActiveLink';
 import * as css from './styles/Home.m.css';
 
+import list from '../generated/list';
 import content from '../generated/sample-tutorial';
 
 export default class Documentation extends WidgetBase {
 	protected render() {
 		return (
 			<div classes={[css.root]}>
-				<Link to='tutorial' params={ { tutorial: 'sample-tutorial' } } activeClasses={['active']}>
-					Sample Tutorial
-				</Link>
+				{ list.map((tutorial) => (
+					<Link to='tutorial' params={ { tutorial } } activeClasses={['active']}>
+						{ tutorial }
+					</Link>
+				)) }
 				<Outlet id="tutorial" renderer={({ params }) => {
 					const { tutorial } = params;
 					console.log(tutorial);
