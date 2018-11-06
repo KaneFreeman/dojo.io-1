dc8b3364d2057b4501a49b0320ced8a5773a6ff5
Matthew Gadd <mgadd@sitepen.com>
2018-11-06 15:10:53 +0000

    Update sample-code.ts

--- content/sample-code.ts
+++ content/sample-code.ts
@@ -2,9 +2,9 @@ import { WidgetBase } from '@dojo/framework/widget-core/WidgetBase';
 import { v, w } from '@dojo/framework/widget-core/d';
 
 export default class App extends WidgetBase {
-	//@start-region renderFunc
+    //@start-region renderFunc
     protected render() {
         return v('div', []);
     }
-	//@end-region renderFunc
+    //@end-region renderFunc
 }
